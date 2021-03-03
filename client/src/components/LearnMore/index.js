5cdb73672a28f219818b37a03b3ad5742845c9c5
kyle <kyle@kyle.nyc>
2021-03-02 20:50:52 -0500

    deleted heading

--- client/src/components/LearnMore/index.js
+++ client/src/components/LearnMore/index.js
@@ -14,7 +14,6 @@ const LearnMore = () => {
     Keep in Touch
     </button> */}
     <div id="seeInfo">
-    <h3>NEWS AND UPDATED WEBSITE COMING SOON.</h3>
     <div id="socialLink"><i style={{fontSize:"24px", color: "rgb(54, 69, 79)", marginRight: "5px"}} class="fa fa-instagram"></i> <a href="https://www.instagram.com/gaspar_rosielstone/">@gaspar_rosielstone</a></div>
     </div>
     </div>
